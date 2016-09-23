ec1d261f3782cd0f9c22b82fe427e6d6849f0560
Trenton Kress <trenton.kress@gmail.com>
2016-09-22 19:18:30 -0700

    Hamburger event handler started

--- class-02-jquery-and-the-dom/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-the-dom/pair-assignment/starter-code/scripts/articleView.js
@@ -3,10 +3,7 @@
 var articleView = {};
 
 articleView.toggleNavDisplay = function() {
-  $('icon-menu').on('click', function() {
-    $('main-nav ul').toggle(500);
-  });
-  // DONE: if you want a challenge, write an event handler to toggle the display property of the nav menu.
+  // TODO: if you want a challenge, write an event handler to toggle the display property of the nav menu.
   // hint: create a 'hidden' class that can be applied to or removed from the nav ul
 };
 
