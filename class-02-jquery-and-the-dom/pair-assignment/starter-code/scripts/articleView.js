48ab6d121005b1d4373c52de0dacf67c2f87bb25
Britney <britney.presley26@gmail.com>
2016-09-22 19:47:11 -0700

    toggle nav display done

--- class-02-jquery-and-the-dom/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-the-dom/pair-assignment/starter-code/scripts/articleView.js
@@ -3,8 +3,17 @@
 var articleView = {};
 
 articleView.toggleNavDisplay = function() {
-  // TODO: if you want a challenge, write an event handler to toggle the display property of the nav menu.
+  // #DONE: if you want a challenge, write an event handler to toggle the display property of the nav menu.
   // hint: create a 'hidden' class that can be applied to or removed from the nav ul
+  $('.icon-menu').on('click', function(e) {
+    var List = $('.main-nav ul');
+    List.toggle();
+    // if( List.hasClass('shown')){
+    //   List.removeClass('shown');
+    // } else {
+    //   List.addClass('shown');
+    // }
+  });
 };
 
 articleView.toggleNavDisplay();
