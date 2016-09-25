c1fdb477822109358116fcd159d7263598485705
Trenton Kress <trenton.kress@gmail.com>
2016-09-25 11:58:09 -0700

    Author data and JS linked to HTML

--- class-03-jquery-and-events/pair-assignment/starter-code/scripts/article.js
+++ class-03-jquery-and-events/pair-assignment/starter-code/scripts/article.js
@@ -16,10 +16,11 @@ Article.prototype.toHtml = function() {
   $newArticle.removeClass('template');
 
   $newArticle.attr('data-category', this.category);
-  // TODO: Use jQuery to also add the author name as a data-attribute of the newly cloned article.
+  // DONE: Use jQuery to also add the author name as a data-attribute of the newly cloned article.
   //       Doing so will allow us to use selectors to target articles, based on who wrote them.
 
   $newArticle.find('.byline a').text(this.author);
+  $newArticle.data('author', this.author);
   $newArticle.find('.byline a').attr('href', this.authorUrl);
   $newArticle.find('h1:first').text(this.title);
   $newArticle.find('.article-body').html(this.body);
