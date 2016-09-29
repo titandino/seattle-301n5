6f4587e3194b03f5ad7572398921ffec84fd2dbe
Trenton Kress <trenton.kress@gmail.com>
2016-09-28 19:50:30 -0700

    Pair programming with Britney finished adding script tag and passing Article object into the handlebars template

--- class-04-templates/pair-assignment/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/starter-code/scripts/article.js
@@ -12,10 +12,10 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // DONE: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-
+  var template = Handlebars.compile($('#article-template').html());
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -23,7 +23,8 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  // DONE: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  return template(this);
 };
 
 rawData.sort(function(a,b) {
