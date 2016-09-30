1761b3f09a3d5d65c9640d7b606d2f48edd79914
Dan Schwartz <dpschwartz@gmail.com>
2016-09-29 20:29:58 -0700

    Finish class05 demo code

--- class-05-new-article-creation/lecture/starter-code/scripts/articleView.js
+++ class-05-new-article-creation/lecture/starter-code/scripts/articleView.js
@@ -5,16 +5,14 @@ var articleView = {};
 
 articleView.populateFilters = function() {
   $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      var val = $(this).find('address a').text();
-      var optionTag = '<option value="' + val + '">' + val + '</option>';
-      $('#author-filter').append(optionTag);
+    var val = $(this).find('address a').text();
+    var optionTag = '<option value="' + val + '">' + val + '</option>';
+    $('#author-filter').append(optionTag);
 
-      val = $(this).attr('data-category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category-filter option[value="' + val + '"]').length === 0) {
-        $('#category-filter').append(optionTag);
-      }
+    val = $(this).attr('data-category');
+    optionTag = '<option value="' + val + '">' + val + '</option>';
+    if ($('#category-filter option[value="' + val + '"]').length === 0) {
+      $('#category-filter').append(optionTag);
     }
   });
 };
@@ -72,34 +70,55 @@ articleView.setTeasers = function() {
 
 articleView.initNewArticlePage = function() {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later.
-
+  $('.tab-content').show();
   // TODO: Any new article we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we
   // have data to export. Also, let's add a focus event to help us select and copy the
   // resulting JSON.
-
+  $('#article-export').hide();
+  $('#article-json').on('focus', function() {
+    this.select();
+  });
   // TODO: Add an event handler to update the preview and the export field if any inputs change.
-
+  $('#new-form').on('change', 'input, textarea', articleView.create);
 };
 
 articleView.create = function() {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #article-preview element, so we can put in the updated preview
+  var newArticle;
+  $('#article-preview').empty();
 
   // TODO: Instantiate an article based on what's in the form fields:
+  var obj = {
+    title: $('#article-title').val(),
+    category: $('#article-category').val(),
+    author: $('#article-author').val(),
+    authorUrl: $('#article-author-url').val(),
+    publishedOn: $('#article-published:checked').length ? new Date() : null,
+    body: $('#article-body').val()
+  };
 
   // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
+  newArticle = new Article(obj);
+  $('#article-preview').append(newArticle.toHtml());
 
   // TODO: Activate the highlighting of any code blocks (ex:
   /*
-  ```
+  ```javascript
   function example() {
     return 'Hooray! Code highlighting!';
   }
   ```
   */
 
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+
   // TODO: Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#article-export').show();
+  $('#article-json').val(JSON.stringify(newArticle) + ',');
 };
 
 
