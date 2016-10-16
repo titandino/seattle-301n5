677f9595399160336ec10d4ada067848a5ece3ee
Trenton Kress <trenton.kress@gmail.com>
2016-10-16 12:19:14 -0700

    Finished all todos

--- class-09-joins-and-relations/lab/zip-codes/scripts/map.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/map.js
@@ -1,10 +1,66 @@
-function initMap() {
+var map;
+
+function initMap(state, city) {
   // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 47.611435, lng: -122.330456},
     scrollwheel: true,
     zoom: 8
   });
 
-  // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
+  var zips = [];
+  if ($('input[name=zip]').val()) {
+    webDB.execute([{
+      'sql': 'SELECT city,latitude,longitude FROM zips WHERE zip=?',
+      'data': [ $('input[name=zip]').val() ]
+    }], function(resultArray) {
+      var markerLocations = resultArray.map(function(result) {
+        return {
+          lat: result.latitude,
+          lng: result.longitude
+        };
+      });
+      loadMarkers(markerLocations);
+      $('form input[type=text]').val('');
+    });
+  }
+
+  if (city && state) {
+    webDB.execute([{
+      'sql': 'SELECT latitude,longitude FROM zips WHERE city=? AND state=?',
+      'data': [ city, state ]
+    }], function(result) {
+      var markerLocations = result.map(function(result) {
+        return {
+          lat: result.latitude,
+          lng: result.longitude
+        };
+      });
+      loadMarkers(markerLocations);
+    });
+  }
+
+  // TODID: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
+
 }
+
+function addMarker(location, label) {
+  var marker = new google.maps.Marker({
+    position: location,
+    label: label
+  });
+  marker.setMap(map);
+  map.setCenter(location);
+}
+
+function loadMarkers(locations, centerMap) {
+  for (var i in locations) {
+    addMarker(locations[i], i + 1);
+  }
+  if (centerMap)
+    map.setCenter(centerMap);
+  else
+    map.setCenter(locations[0]);
+}
+
+Search.initAll();
--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -1,7 +1,55 @@
 (function(module) {
 
-  // TODO: Write the code to populate your filters, and enable the search queries here in search.js
-  // TODO: You will also interact with the map.js file here
+  // TODID: Write the code to populate your filters, and enable the search queries here in search.js
+  // TODID: You will also interact with the map.js file here
+  function Search() { }
 
+  function setupZipSearch() {
+    $('form').on('submit', function(e) {
+      e.preventDefault();
+      if ($('input[name=zip]').val()) {
+        initMap();
+      }
+    });
+  }
 
+  function setupStates() {
+    webDB.execute([{
+      'sql': 'SELECT DISTINCT state FROM zips ORDER BY state'
+    }], function(result) {
+      var states = result.map(function(obj) {
+        return obj.state;
+      });
+      states.filter(function(curr, i) {
+        return states.indexOf(curr) == i;
+      }).forEach(function(curr) {
+        $('#state-select').append('<option>' + curr + '</option>');
+      });
+    });
+    $('#state-select').on('change', function() {
+      webDB.execute([{
+        'sql': 'SELECT DISTINCT city,latitude,longitude FROM zips WHERE state=? ORDER BY city',
+        'data': [$('#state-select').val()]
+      }], function(result) {
+        var cities = result.map(function(obj) {
+          return obj.city;
+        })
+        cities.filter(function(curr, i) {
+          return cities.indexOf(curr) == i;
+        }).forEach(function(curr) {
+          $('#city-select').append('<option>' + curr + '</option>');
+        });
+      });
+    });
+    $('#city-select').on('change', function() {
+      initMap($('#state-select').val(), $('#city-select').val());
+    });
+  }
+
+  Search.initAll = function() {
+    setupZipSearch();
+    setupStates();
+  }
+
+  module.Search = Search;
 })(window)
