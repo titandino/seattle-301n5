12c50b13daaa4f38bb5e428ed5127e67d3801223
Scott Schmidt <sjschmidt44@gmail.com>
2016-10-30 08:57:06 -0700

    Unzip class13/14 starter code

---
+++ class-13-production-deployment/pair-assignment/starter-code/server.js
@@ -0,0 +1,31 @@
+var requestProxy = require('express-request-proxy'),
+  express = require('express'),
+  port = process.env.PORT || 3000,
+  app = express();
+
+// var proxyGitHub = function(request, response) {
+//   console.log('Routing GitHub request for', request.params[0]);
+//   (requestProxy({
+//     url: 'https://api.github.com/' + request.params[0],
+//     headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+//   }))(request, response);
+// };
+
+app.get('/github/*', function(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+  }));
+});
+
+app.use(express.static('./'));
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.' });
+});
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port + '!');
+});
